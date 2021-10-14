a801d66e47a120ccdf9474635a126d5c33449abe
Jacob Brown <jtb68@uclive.ac.nz>
2021-10-15 09:32:10 +1300

    Updated exisiting code to use task.h void *data instead of global variables

--- game.c
+++ game.c
@@ -4,17 +4,25 @@
 #include "task.h"
 #include "navswitch.h"
 
-static uint16_t player_position[] = {3, 6};
-static uint16_t prev_player_position[2];
+typedef struct  {
+    uint16_t player_position[2];
+    uint16_t prev_player_position[2];
+} game_data_t;
 
-static void task_update_player (void) {
-    prev_player_position[0] = player_position[0];
-    prev_player_position[1] = player_position[1];
-    update_movement(player_position);
+static void task_update_player (void *data) {
+    
+    game_data_t* game_data = data;
+
+    game_data->prev_player_position[0] = game_data->player_position[0];
+    game_data->prev_player_position[1] = game_data->player_position[1];
+    update_movement(game_data->player_position);
 }
 
-static void task_draw_screen (void) {
-    draw_player(player_position, prev_player_position);
+static void task_draw_screen (void *data) {
+
+    game_data_t* game_data = data;
+
+    draw_player(game_data->player_position, game_data->prev_player_position);
     tinygl_update();
 }
 
@@ -33,13 +41,14 @@ int main (void)
     tinygl_draw_line(tinygl_point(4,0), tinygl_point (4, 6), 1);
 
     //Create the tuple used for the coordinates of the player. Set it to 3,6 by default.
-    
-    tinygl_draw_point(tinygl_point(player_position[0], player_position[1]), 1);
+    static game_data_t game_data;
+    game_data.player_position[0] = 3;
+    game_data.player_position[1] = 6;
 
     task_t tasks[] =
     {
-        {.func = task_update_player, .period = TASK_RATE / 5.},
-        {.func = task_draw_screen, .period = TASK_RATE / 1000.},
+        {.func = task_update_player, .period = TASK_RATE / 5., .data = &game_data},
+        {.func = task_draw_screen, .period = TASK_RATE / 1000., .data = &game_data},
     };
 
     task_schedule(tasks, 2);
